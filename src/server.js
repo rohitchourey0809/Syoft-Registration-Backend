7ced062b525a128fd2486aaf6c1bd8a1b5ddf06b
ROHIT CHOUREY <97465195+rohitchourey0809@users.noreply.github.com>
2024-05-31 17:22:11 +0530

    fixed

--- src/server.js
+++ src/server.js
@@ -30,7 +30,7 @@ app.listen(PORT, async () => {
   try {
     await connect();
     console.log("Connected to the database");
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${"http://localhost:" + PORT}`);
   } catch (err) {
     console.error("Error connecting to the database:", err);
   }
